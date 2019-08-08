2ab296ec9556c7efff21bc8afb6cec2d132a977a
Suriyapong Thinsuk <suriyapongt@wisesoft.co.th>
2019-08-08 19:33:10 +0700

    update demo

--- src/demo/app/app.component.ts
+++ src/demo/app/app.component.ts
@@ -9,4 +9,17 @@ export class AppComponent {
     title = 'number-format-base';
     example1: number;
     example2: number;
+    example3: string  = '#,###,###.##';
+    config: string;
+    configInput: string;
+
+    constructor() {
+        this.setConfig();
+    }
+
+    setConfig() {
+        this.config = this.example3;
+        this.configInput = null;
+    }
+
 }
--- src/ngx-number-format/lib/ngx-number-format.directive.ts
+++ src/ngx-number-format/lib/ngx-number-format.directive.ts
@@ -27,6 +27,7 @@ export class NgxNumberFormatDirective implements ControlValueAccessor, OnInit {
     private _detectDelete: boolean = false;
     private _detectBackspace: boolean = false;
     private _detectSelectAll: boolean = false;
+    private _process: boolean = false;
 
     public onChange = (_: any) => { };
     public onTouch = () => { };
@@ -39,72 +40,59 @@ export class NgxNumberFormatDirective implements ControlValueAccessor, OnInit {
 
     @Input('ngxNumberFormat')
     public set initialize(_value: string) {
-
-        if (_value.indexOf(',') !== -1) {
-            this._format = true;
-        }
-        _value = _value.replace(/,/g, '');
-        let data = _value.split('.');
-
-        if (data.length == 1) {
-            this._max = data[0].length;
-        } else if (data.length == 2) {
-            this._max = data[0].length;
-            this._decimal = data[1].length;
-        } else {
-            throw new Error("Wrong format number.");
-        }
-
+        this.setFormat(_value);
     }
 
     @HostListener('keydown', ['$event'])
     onKeyDown(event: KeyboardEvent) {
 
-        if (event.key == 'Backspace') {
-            this._detectBackspace = true;
-            let last: string = this._formElement.value.substring(this._formElement.selectionStart - 1, this._formElement.selectionStart);
-            if (this._formElement.selectionStart == this._formElement.selectionEnd && last == ',') {
-                this.setCursorAt(this._formElement.selectionStart - 1);
-                event.preventDefault();
+        if (this._process) {
+            if (event.key == 'Backspace') {
+                this._detectBackspace = true;
+                let last: string = this._formElement.value.substring(this._formElement.selectionStart - 1, this._formElement.selectionStart);
+                if (this._formElement.selectionStart == this._formElement.selectionEnd && last == ',') {
+                    this.setCursorAt(this._formElement.selectionStart - 1);
+                    event.preventDefault();
+                }
+            } else {
+                this._detectBackspace = false;
             }
-        } else {
-            this._detectBackspace = false;
-        }
-
-        if (event.key == 'Delete') {
-            this._detectDelete = true;
-            let last: string = this._formElement.value.substring(this._formElement.selectionEnd, this._formElement.selectionEnd + 1);
-            if (this._formElement.selectionStart == this._formElement.selectionEnd && last == ',') {
-                this.setCursorAt(this._formElement.selectionEnd + 1);
-                event.preventDefault();
+    
+            if (event.key == 'Delete') {
+                this._detectDelete = true;
+                let last: string = this._formElement.value.substring(this._formElement.selectionEnd, this._formElement.selectionEnd + 1);
+                if (this._formElement.selectionStart == this._formElement.selectionEnd && last == ',') {
+                    this.setCursorAt(this._formElement.selectionEnd + 1);
+                    event.preventDefault();
+                }
+            } else {
+                this._detectDelete = false;
             }
-        } else {
-            this._detectDelete = false;
-        }
-
-        if (
-            this._specialKeys.indexOf(event.key) !== -1
-            || (event.keyCode === 65 && (event.ctrlKey || event.metaKey)) // Allow: Ctrl + A
-            || (event.keyCode === 67 && (event.ctrlKey || event.metaKey)) // Allow: Ctrl + C
-            || (event.keyCode === 86 && (event.ctrlKey || event.metaKey)) // Allow: Ctrl + V
-            || (event.keyCode === 88 && (event.ctrlKey || event.metaKey)) // Allow: Ctrl + X
-        ) {
-            return;
-        }
-
-        let current: string = this.el.nativeElement.value;
-        let firstPart: string = current.substring(0, this._formElement.selectionStart);
-        let secondPart: string = current.substring(this._formElement.selectionEnd);
-        let next: string = (firstPart.concat(event.key) + secondPart).replace(/,/g, '');
-
-        let regEx = this.getRegEx();
-
-        let value = next.split('.');
-        if (next && !String(next).match(regEx) || (value[0].length > this._max && this._formElement.selectionStart == this._formElement.selectionEnd) || (this._decimal > 0 && value.length == 2 && (value[1].length > this._decimal && this._formElement.selectionStart == this._formElement.selectionEnd))) {
-            event.preventDefault();
-        } else {
-            this._oldSelectionStart = this._formElement.selectionStart;
-            this._oldValue = this._formElement.value;
+    
+            if (
+                this._specialKeys.indexOf(event.key) !== -1
+                || (event.keyCode === 65 && (event.ctrlKey || event.metaKey)) // Allow: Ctrl + A
+                || (event.keyCode === 67 && (event.ctrlKey || event.metaKey)) // Allow: Ctrl + C
+                || (event.keyCode === 86 && (event.ctrlKey || event.metaKey)) // Allow: Ctrl + V
+                || (event.keyCode === 88 && (event.ctrlKey || event.metaKey)) // Allow: Ctrl + X
+            ) {
+                return;
+            }
+    
+            let current: string = this.el.nativeElement.value;
+            let firstPart: string = current.substring(0, this._formElement.selectionStart);
+            let secondPart: string = current.substring(this._formElement.selectionEnd);
+            let next: string = (firstPart.concat(event.key) + secondPart).replace(/,/g, '');
+    
+            let regEx = this.getRegEx();
+    
+            let value = next.split('.');
+            if (next && !String(next).match(regEx) || (value[0].length > this._max && this._formElement.selectionStart == this._formElement.selectionEnd) || (this._decimal > 0 && value.length == 2 && (value[1].length > this._decimal && this._formElement.selectionStart == this._formElement.selectionEnd))) {
+                event.preventDefault();
+            } else {
+                this._oldSelectionStart = this._formElement.selectionStart;
+                this._oldValue = this._formElement.value;
+            }
         }
 
     }
@@ -120,14 +108,16 @@ export class NgxNumberFormatDirective implements ControlValueAccessor, OnInit {
     public onInput(event): void {
         let value = event.target.value;
 
-        if (value && !String(value).replace(/,/g, '').match(this.getRegEx())) {
-            this._formElement.value = '';
-        } else {
-            if (this._detectBackspace || this._detectDelete) {
-                this._oldSelectionStart = this._formElement.selectionStart - 1;
-                this._oldValue = this._formElement.value;
+        if (this._process) {
+            if (value && !String(value).replace(/,/g, '').match(this.getRegEx())) {
+                this._formElement.value = '';
+            } else {
+                if (this._detectBackspace || this._detectDelete) {
+                    this._oldSelectionStart = this._formElement.selectionStart - 1;
+                    this._oldValue = this._formElement.value;
+                }
+                this.onValueChange(value);
             }
-            this.onValueChange(value);
         }
 
     }
@@ -198,7 +188,7 @@ export class NgxNumberFormatDirective implements ControlValueAccessor, OnInit {
 
     private onValueChange(newValue: string, cursor: boolean = true) {
 
-        if (newValue !== this._displayValue) {
+        if (newValue !== this._displayValue && this._process) {
             let value;
 
             if ((newValue == null) || (newValue.trim() === '')) {
@@ -235,5 +225,30 @@ export class NgxNumberFormatDirective implements ControlValueAccessor, OnInit {
 
     private getRegEx(): RegExp {
         return (this._decimal > 0) ? this._regExNumberAndDecimal : this._regExNumber;
+    }
+
+    private setFormat(_value: string) {
+
+        if (_value) {
+            this._process = true;
+        } else {
+            this._process = false;
+        }
+
+        if (_value.indexOf(',') !== -1) {
+            this._format = true;
+        } else {
+            this._format = false;
+        }
+        _value = _value.replace(/,/g, '');
+        let data = _value.split('.');
+
+        if (data.length == 1) {
+            this._max = data[0].length;
+        } else if (data.length == 2) {
+            this._max = data[0].length;
+            this._decimal = data[1].length;
+        }
+
     }
 }
