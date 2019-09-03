b308f0819cd069e2e40bbc9a573f8484edd33f01
Suriyapong <suriyapongt@wisesoft.co.th>
2019-09-03 12:47:13 +0700

    Auto-generated commit

--- ngx-number-format/esm5/public-api.js
+++
@@ -1,9 +0,0 @@
-/**
- * @fileoverview added by tsickle
- * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
- */
-/*
- * Public API Surface of ngx-number-format
- */
-export { NgxNumberFormatModule } from './lib/ngx-number-format.module';
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoicHVibGljLWFwaS5qcyIsInNvdXJjZVJvb3QiOiJuZzovL25neC1udW1iZXItZm9ybWF0LyIsInNvdXJjZXMiOlsicHVibGljLWFwaS50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOzs7Ozs7O0FBSUEsc0NBQWMsZ0NBQWdDLENBQUMiLCJzb3VyY2VzQ29udGVudCI6WyIvKlxuICogUHVibGljIEFQSSBTdXJmYWNlIG9mIG5neC1udW1iZXItZm9ybWF0XG4gKi9cblxuZXhwb3J0ICogZnJvbSAnLi9saWIvbmd4LW51bWJlci1mb3JtYXQubW9kdWxlJztcbiJdfQ==
--- ngx-number-format/ngx-number-format.d.ts
+++
@@ -1,5 +0,0 @@
-/**
- * Generated bundle index. Do not edit.
- */
-export * from './public-api';
-export { NgxNumberFormatDirective as ɵa } from './lib/ngx-number-format.directive';
